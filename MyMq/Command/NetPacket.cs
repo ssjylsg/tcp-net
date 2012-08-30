996e3dbb2b412d4e2ab1b2e83e6609576011c192
ssjylsg <ssjylsg@126.com>
2012-08-30 18:18:27 +0800

    update

--- MyMq/Command/NetPacket.cs
+++ MyMq/Command/NetPacket.cs
@@ -17,11 +17,17 @@ namespace MyMq
         }
         private ICommand _command;
         private NetPacketHead _packetHead;
+        /// <summary>
+        /// 包体
+        /// </summary>
         public ICommand Command
         {
             get { return _command; }
             set { _command = value; }
         }
+        /// <summary>
+        /// 包头
+        /// </summary>
         public NetPacketHead PacketHead
         {
             get { return _packetHead; }
