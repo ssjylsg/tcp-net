bd18009ac4758fd2460a10772f305de02fb96c88
ssjylsg <ssjylsg@126.com>
2012-08-31 18:04:16 +0800

    update

---
+++ PubSubServer/ServerForm.Designer.cs
@@ -0,0 +1,92 @@
+﻿namespace PubSubServer
+{
+    partial class ServerForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.clearBtn = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.DarkRed;
+            this.label1.Location = new System.Drawing.Point(64, 26);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(293, 25);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Topic Based Pub/Sub ServerForm";
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom)
+                        | System.Windows.Forms.AnchorStyles.Left)
+                        | System.Windows.Forms.AnchorStyles.Right)));
+            this.richTextBox1.Location = new System.Drawing.Point(25, 68);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(474, 268);
+            this.richTextBox1.TabIndex = 1;
+            this.richTextBox1.Text = "";
+            // 
+            // clearBtn
+            // 
+            this.clearBtn.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.clearBtn.Location = new System.Drawing.Point(404, 342);
+            this.clearBtn.Name = "clearBtn";
+            this.clearBtn.Size = new System.Drawing.Size(75, 23);
+            this.clearBtn.TabIndex = 2;
+            this.clearBtn.Text = "清空";
+            this.clearBtn.UseVisualStyleBackColor = true;
+            this.clearBtn.Click += new System.EventHandler(this.clearBtn_Click);
+            // 
+            // ServerForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(521, 368);
+            this.Controls.Add(this.clearBtn);
+            this.Controls.Add(this.richTextBox1);
+            this.Controls.Add(this.label1);
+            this.Name = "ServerForm";
+            this.Text = "ServerForm";
+            this.Load += new System.EventHandler(this.Server_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.Button clearBtn;
+    }
+}
+
---
+++ PubSubServer/ServerForm.cs
@@ -0,0 +1,55 @@
+﻿using System.Windows.Forms;
+using MyMq;
+
+namespace PubSubServer
+{
+    public partial class ServerForm : Form
+    {
+        public ServerForm()
+        {
+            InitializeComponent();
+            HostPublishSubscribeServices();
+        }
+
+        private IService _producterService; // 发布服务
+        private IService _subscriberService; // 订阅服务
+        private void HostPublishSubscribeServices()
+        {
+            ProducerTcpService.ReceiveMessageEventHandler += new ReceiveMessageEventHandler(ProducerTcpService_ReceiveMessageEventHandler);
+            _producterService = new ProducerTcpService();
+
+            _producterService.StartService();
+            _subscriberService = new TcpSubscriberService();
+            _subscriberService.StartService();
+
+        }
+
+        private int _messageCount = 1; // 消息计数器
+        void ProducerTcpService_ReceiveMessageEventHandler(object message)
+        {
+            if (this.richTextBox1.InvokeRequired)
+            {
+                richTextBox1.Invoke(
+                    new MethodInvoker(
+                        delegate()
+                        {
+                            this.richTextBox1.AppendText(string.Format("第{1}条数据:{0}\r\n", message.ToString(),
+                                                                       _messageCount++));
+                        }));
+            }
+        }
+
+        private void Server_Load(object sender, System.EventArgs e)
+        {
+
+        }
+        /// <summary>
+        /// 清空数据
+        /// </summary>
+        private void clearBtn_Click(object sender, System.EventArgs e)
+        {
+            this._messageCount = 1;
+            this.richTextBox1.Clear();
+        }
+    }
+}
